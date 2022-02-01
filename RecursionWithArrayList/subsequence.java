0656798ae14e95752069f95e56883ecb7c3eaeef
NilanjanJana16 <nildbs16@gmail.com>
2022-02-01 14:44:35 +0530

    1st commit

--- RecursionWithArrayList/subsequence.java
+++ RecursionWithArrayList/subsequence.java
@@ -1,5 +1,45 @@
 package RecursionWithArrayList;
 
+import java.io.*;
+import java.util.*;
+
 public class subsequence {
-    
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        ArrayList<String> res = gss(str);
+        System.out.println(res);
+
+    }
+
+
+    public static ArrayList<String> gss(String str) {
+
+        if(str.length() ==0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+            }   
+        
+
+        char ch = str.charAt(0);
+        String rremos = str.substring(1);
+        ArrayList<String> rres = gss(rremos);
+
+        ArrayList<String> mres = new ArrayList<>();
+        for(String val: rres){
+            mres.add(""+ val);
+            
+        }
+         for(String val: rres){
+           
+            mres.add(ch+ val);
+        }
+
+        return mres;
+    }
+
+
 }
